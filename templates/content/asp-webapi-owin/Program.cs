dd60c871c3428c8f6a641b08613ab34dbdbd26db
José Medrano <jose.medrano@xamarin.com>
2018-04-22 00:18:28 +0200

    Adds template for AspNet WebApi Owin

---
+++ templates/content/asp-webapi-owin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace NewApp
+{
+	class Program
+	{
+		public static void Main (string[] args)
+		{
+			var port = "8085";
+			var config = new HttpSelfHostConfiguration($"http://localhost:{port}");
+			config.MapHttpAttributeRoutes();
+
+			using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+			{
+				server.OpenAsync().Wait();
+				Console.WriteLine($"Started host server in {port}.");
+				while (true) {
+					Thread.Sleep(1000);
+				}
+			}
+		}
+	}
+}
---
+++ templates/content/asp-webapi-owin/ServiceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewApp
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class BookServiceController : ApiController
+	{
+		readonly List<Book> data = new List<Book> ();
+
+		public BookServiceController () 
+		{
+			data.Add (new Book { Id = 1, Name = "Test1" });
+			data.Add (new Book { Id = 2, Name = "Test2" });
+		}
+
+		[Route ("api/books")]
+		public IEnumerable<Book> GetBooks () 
+		{
+			return data;
+		}
+
+		[Route ("api/books/{id:int}")]
+		public Book GetBook (int id)
+		{
+			return data.FirstOrDefault (s => s.Id == id);
+		}
+
+		[Route ("api/books")]
+		[HttpPost]
+		public HttpResponseMessage CreateBook (Book book) 
+		{
+			data.Add (book);
+			return new HttpResponseMessage (HttpStatusCode.OK);
+		}
+	}
+}
