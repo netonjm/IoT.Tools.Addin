52208af7610ae0b4a56c05ba2863278dd0cabdbd
Jose Medrano <jose.medrano@microsoft.com>
2018-04-28 14:17:30 +0200

    Includes templates for Forms and Gtk in FSharp

---
+++ templates/content/forms-fsharp/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace NewApp
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Platform.GTK
+
+module Program =
+
+[<EntryPoint>]
+let main (argv :string[]) = 
+    Gtk.Application.Init ()
+    Forms.Init ()
+    let app = new App ()
+    let window = new FormsWindow ()
+    window.LoadApplication (app)
+    window.Show ()
+    Gtk.Application.Run ()
+    0
---
+++ templates/content/gtk-fsharp/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace NewApp
+
+open System
+open Gtk
+
+module Program =
+
+[<EntryPoint>]
+let main (argv :string[]) = 
+
+    let width, height = 500, 500
+    Application.Init ()
+
+    let window = new Window ("FSharp IoT Example")
+    window.SetDefaultSize (width, height)
+    window.DeleteEvent.Add (fun e -> window.Hide(); Application.Quit(); e.RetVal <- true)
+    let mutable count = 0
+    let button = new Button ("I'm a Raspberry. Click Here!")
+    button.Clicked.Add (fun (s) -> count <- count + 1; button.Label <- String.Format("You pressed {0} times",string count))
+    window.Add (button)
+
+    window.ShowAll()
+    window.Show()
+    Application.Run ()
+    0
